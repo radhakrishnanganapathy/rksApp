9fcab9560e36fa5a86645f9ea5cc336fd0c78a81
rkredblox <radhakrishnanganapathyr@gmail.com>
2025-12-17 18:48:26 +0530

    chore: Trigger new deploy to fix path error

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+console.log("Starting RKS Server (Reverted Version)...");
 
 app.use(cors());
 app.use(express.json());
